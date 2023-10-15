62dc552bfd9c3e02e5dc053abf667b49646dc16f
Leandro <leandro_marangoni@outlook.com>
2023-10-14 23:46:45 -0300

    Atualizado para a puppeteer

--- frontend/screens/index/index.js
+++ frontend/screens/index/index.js
@@ -2,6 +2,8 @@ const { getAllProducts } = require("../../../database.js");
 const fs = require('fs');
 const htmlToPdf = require('html-pdf');
 const path = require('path');
+const puppeteer = require('puppeteer');
+
 
 function getImageSrc(produto) {
     return produto.imagem ? `data:image/png;base64,${Buffer.from(produto.imagem).toString('base64')}` : 'path/to/default-image.png';
@@ -39,13 +41,15 @@ function createCardHTML(produto, index) {
 }
 
 async function exportPdf() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
     const cssContent = readCssFile();
     let htmlContent = readHtmlFile() + '<style>' + cssContent + '</style>';
 
     try {
         const rows = await getAllProducts();
         if (rows && rows.length > 0) {
-            // htmlContent = htmlContent + rows.map(createCardHTML).join('');
             const cardsHTML = rows.map((produto, index) => createCardHTML(produto, index)).join('');
             htmlContent = htmlContent + cardsHTML;
         }
@@ -53,20 +57,19 @@ async function exportPdf() {
         handleDatabaseError(error);
     }
 
-    // htmlContent = "</div></main><script src='./home.js'></script></body></html>" + htmlContent;
     htmlContent = "</tr></table></body></html>" + htmlContent;
 
     if (htmlContent.trim() !== '') {
         const pdfOptions = {
+            path: 'Catalogo.pdf',
             format: 'Letter'
         };
-        htmlToPdf.create(htmlContent, pdfOptions).toFile('Catalogo.pdf', (err, res) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log('PDF gerado com sucesso:', res);
-        });
+        
+        await page.setContent(htmlContent);
+        await page.pdf(pdfOptions);
+
+        await browser.close();
+        console.log('PDF gerado com sucesso');
     } else {
         console.error('O conteúdo HTML está vazio. Nenhum PDF será gerado.');
     }
