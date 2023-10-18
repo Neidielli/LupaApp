e83d76b377ab3255743999d75c03b4b41a4d5473
Leandro <leandro_marangoni@outlook.com>
2023-10-18 08:35:11 -0300

    Tentativa de passar PDF para main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const { app, BrowserWindow } = require('electron');
+const { getAllProducts } = import('./database.js');
+const puppeteer = require('puppeteer-core');
+const fs = require('fs');
+const path = require('path');
 
 function createMainWindow() {
   let mainWindow = new BrowserWindow({
@@ -8,6 +12,7 @@ function createMainWindow() {
       nodeIntegration: true,
       contextIsolation: false,
     },
+    preload: path.join(__dirname, 'preload.js'),
   });
 
   mainWindow.loadFile('frontend/screens/index/index.html');
@@ -21,10 +26,81 @@ function createMainWindow() {
 
 app.on('ready', () => {
   const mainWindow = createMainWindow();
-
 });
 
 app.on('window-all-closed', () => {
-  
   app.quit();
 });
+
+async function exportPdf() {
+  const executablePath = app.getPath('exe');
+  const browser = await puppeteer.launch(executablePath);
+  const page = await browser.newPage();
+
+  const cssContent = readCssFile();
+  let htmlContent = readHtmlFile() + '<style>' + cssContent + '</style>';
+
+  try {
+    const rows = await getAllProducts();
+    console.log(rows)
+    if (rows && rows.length > 0) {
+      const cardsHTML = rows.map((produto, index) => createCardHTML(produto, index)).join('');
+      htmlContent = htmlContent + cardsHTML;
+    }
+  } catch (error) {
+    handleDatabaseError(error);
+  }
+
+  htmlContent = htmlContent + "</tr></table></body></html>";
+
+  if (htmlContent.trim() !== '') {
+    const pdfOptions = {
+      path: 'Catalogo.pdf',
+      preferCSSPageSize: true,
+      printBackground: true
+    };
+
+    await page.setContent(htmlContent);
+    await page.pdf(pdfOptions);
+
+    await browser.close();
+    console.log('PDF gerado com sucesso');
+  } else {
+    console.error('O conteúdo HTML está vazio. Nenhum PDF será gerado.');
+  }
+
+  function readCssFile() {
+    const cssFilePath = path.join(__dirname, './template.css');
+    return fs.readFileSync(cssFilePath, 'utf-8');
+  }
+
+  function readHtmlFile() {
+    const htmlFilePath = path.join(__dirname, './template.html');
+    return fs.readFileSync(htmlFilePath, 'utf-8');
+  }
+
+  function getImageSrc(produto) {
+    return produto.imagem ? `data:image/png;base64,${Buffer.from(produto.imagem).toString('base64')}` : 'path/to/default-image.png';
+  }
+
+  function createCardHTML(produto, index) {
+    const imageSrc = getImageSrc(produto);
+
+    let cardHTML = `
+    <td>
+        <div class="itemProdutos">
+            <h1>${produto.produto}</h1>
+            <h4>${produto.marca}</h4>
+            <button style="background-color: #23561b; width: 100px; height: 25px; border: none; color: #FFFFFF; margin-bottom: 5px; border-radius: 5px;">R$ ${produto.preco}</button>
+            <img src="${imageSrc}" width="175" height="150">
+        </div>
+    </td>
+    `;
+
+    if ((index + 1) % 3 === 0) {
+      cardHTML += '</tr><tr>';
+    }
+
+    return cardHTML;
+  }
+}
--- preload.js
+++ preload.js
@@ -1,5 +1,15 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  ipcRenderer: ipcRenderer
+  exportPdf: () => {
+    ipcRenderer.send('exportPdf');
+  }
+});
+
+ipcRenderer.on('pdf-exported', (event, status, error) => {
+  if (status === 'success') {
+    console.log('PDF gerado com sucesso');
+  } else if (status === 'error') {
+    console.error('Erro ao gerar PDF:', error);
+  }
 });
