9951368ba70341b5091ea793dadfc9b8bd19d984
Leandro <leandro_marangoni@outlook.com>
2023-10-22 12:32:09 -0300

    Refatoração da geração PDF

--- main.js
+++ main.js
@@ -30,40 +30,24 @@ app.on('ready', () => {
       document.getElementById("exportPdfButton").addEventListener("click", async () => {
         const { getAllProducts } = require('./../../../database.js');
         const puppeteer = require('puppeteer-core');
-        console.log(__dirname)
-        const browser = await puppeteer.launch({ executablePath: path.join(__dirname, 'chrome-win64','chrome.exe') });
-        const page = await browser.newPage();
-
-        const cssContent = readCssFile();
-        let htmlContent = readHtmlFile() + '<style>' + cssContent + '</style>';
 
         try {
-            const rows = await getAllProducts();
-            if (rows && rows.length > 0) {
-                const cardsHTML = rows.map((produto, index) => createCardHTML(produto, index)).join('');
-                htmlContent = htmlContent + cardsHTML;
-            }
+            const browser = await puppeteer.launch({ executablePath: path.join(__dirname, 'chrome-win64', 'chrome.exe') });
+            const page = await browser.newPage();
+
+            let htmlContent = readFile("html") + '<style>' + readFile("css") + '</style>';
+            htmlContent = fillCards(htmlContent, await getAllProducts());
+            await page.setContent(htmlContent + "</tr></table></body></html>");
+            await page.pdf({
+              path: path.join(__dirname, '..', '..', '..', '..', '..', '..', 'Catalogo.pdf'),
+              preferCSSPageSize: true,
+              printBackground: true});
+            await browser.close();
+            console.log('PDF gerado com sucesso');
         } catch (error) {
             handleDatabaseError(error);
         }
-
-        htmlContent =  htmlContent + "</tr></table></body></html>";
-        if (htmlContent.trim() !== '') {
-            const pdfOptions = {
-                path:  path.join(__dirname,'..','..','..','..','..','..','Catalogo.pdf'),
-                preferCSSPageSize: true,
-                printBackground: true
-            };
-            
-            await page.setContent(htmlContent);
-            await page.pdf(pdfOptions);
-
-            await browser.close();
-            console.log('PDF gerado com sucesso');
-        } else {
-            console.error('O conteúdo HTML está vazio. Nenhum PDF será gerado.');
-        }
-            });
+       });
     `);
   });
 });
@@ -72,37 +56,33 @@ app.on('window-all-closed', () => {
   app.quit();
 });
 
-function getImageSrc(produto) {
-  return produto.imagem ? `data:image/png;base64,${Buffer.from(produto.imagem).toString('base64')}` : 'path/to/default-image.png';
-}
-
-function readCssFile() {
-  const cssFilePath = path.join(__dirname, './template.css');
-  return fs.readFileSync(cssFilePath, 'utf-8');
-}
-
-function readHtmlFile() {
-  const htmlFilePath = path.join(__dirname, './template.html');
-  return fs.readFileSync(htmlFilePath, 'utf-8');
-}
-
 function createCardHTML(produto, index) {
-  const imageSrc = getImageSrc(produto);
-
   let cardHTML = `
-  <td>
-      <div class="itemProdutos">
-          <h1>${produto.produto}</h1>
-          <h4>${produto.marca}</h4>
-          <button style="background-color: #23561b; width: 100px; height: 25px; border: none; color: #FFFFFF; margin-bottom: 5px; border-radius: 5px;">R$ ${produto.preco}</button>
-          <img src="${imageSrc}" width="175" height="150">
-      </div>
-  </td>
-  `;
-
+    <td>
+        <div class="itemProdutos">
+            <h1>${produto.produto}</h1>
+            <h4>${produto.marca}</h4>
+            <button style="background-color: #23561b; width: 100px; height: 25px; border: none; color: #FFFFFF; margin-bottom: 5px; border-radius: 5px;">R$ ${produto.preco}</button>
+            <img src="${getImageSrc(produto)}" width="175" height="150">
+        </div>
+    </td>`;
   if ((index + 1) % 3 === 0) {
     cardHTML += '</tr><tr>';
   }
 
   return cardHTML;
+}
+
+function fillCards(htmlContent, rows) {
+  if (rows && rows.length > 0) {
+    return htmlContent + rows.map((produto, index) => createCardHTML(produto, index)).join('');
+  }
+}
+
+function getImageSrc(produto) {
+  return produto.imagem ? `data:image/png;base64,${Buffer.from(produto.imagem).toString('base64')}` : 'path/to/default-image.png';
+}
+
+function readFile(tipo) {
+  return fs.readFileSync(path.join(__dirname, './template.' + tipo), 'utf-8');
 }
